e2fb6a71b04db57c77738e4e99583744ddec70bc
Wellan rayan benvindo <wellanbenvindo25@gmail.com>
2023-09-21 17:47:13 -0300

    Create cad.php

---
+++ ex004/cad.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resutado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado do processamento !!!</h1>
+    </header>
+
+    <main>
+        <?php 
+            $nome = $_GET["nome"];
+            $sobrenome = $_GET["sobrenome"];
+
+            echo "<p> É um prazer te conhecer $nome $sobrenome este é o meu site !!! </p>";
+        ?>
+
+        <p>
+           <a href="javascript:history.go(-1)"> Voltar a Página anterior</a>
+        </p>
+    </main>
+</body>
+</html>
