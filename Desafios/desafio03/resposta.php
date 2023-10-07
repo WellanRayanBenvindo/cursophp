ff951da5dbedf6560b5ab4e903b7afda44f68ccc
Wellan rayan benvindo <wellanbenvindo25@gmail.com>
2023-10-07 16:30:55 -0300

    novodesafio

---
+++ Desafios/desafio03/resposta.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor de moedas </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Conversor de moedas</h1>
+        <?php 
+        //cotação copiada do google dia 07/10/2023
+            $cotação = 5.15;
+        // quantos $$ voce tem ?
+            $dolar = 1000;
+
+        // equivalencia do dolar 
+
+        $dolar = $cotação / $dolar;
+
+
+            echo "Voce tem $valor em sua carteira."
+
+            
+        
+        ?>
+    </main>
+</body>
+</html>
